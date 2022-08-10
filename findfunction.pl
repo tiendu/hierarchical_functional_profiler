dad3285076878e1f3a00cc19874a46e490bcd171
tiendu <52783290+tiendu@users.noreply.github.com>
2022-08-10 13:18:18 +0900

    Add files via upload

---
+++ findfunction.pl
@@ -0,0 +1,37 @@
+use strict;
+
+my $uniprot_brite_path = shift @ARGV;
+my $nr_result_path = shift @ARGV;
+my $nr_result_path_cp = $nr_result_path;
+$nr_result_path_cp =~ s/.*\///;
+my ($file_name, $file_extension) = $nr_result_path_cp =~ /^(.+)\.([^.]+)$/;
+
+open my $uniprot_brite, "<:utf8", $uniprot_brite_path or die;
+my %uniprot_brite_hash;
+while (<$uniprot_brite>) {
+    chomp;
+    my @fields = split "\t";
+    my $uniprot = $fields[0];
+    my $infos = join("\t", @fields[1 .. $#fields]);
+    $uniprot_brite_hash{$uniprot} = $infos unless exists $uniprot_brite_hash{$uniprot};
+    last if eof $uniprot_brite;
+};
+close $uniprot_brite;
+
+open my $nr_result, "<:utf8", $nr_result_path or die;
+open my $functional_profile, ">:utf8", "func_${file_name}.${file_extension}";
+my @records;
+while (<$nr_result>) {
+    chomp;
+    my @fields = split "\t";
+    my $uniprot = $fields[1];
+    my $id = $fields[0];
+    my $start = $fields[6];
+    my $end = $fields[7];
+    if ($uniprot_brite_hash{$uniprot} ne "") {
+        print $functional_profile "$id\t$start\t$end\t$uniprot_brite_hash{$uniprot}\n";
+    };
+    last if eof $nr_result;
+};
+close $nr_result;
+close $functional_profile;

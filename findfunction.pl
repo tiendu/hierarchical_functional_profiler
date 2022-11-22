46b2fc52ca7bd85e817b29b757fda0a8dd523247
Tien Du <52783290+tiendu@users.noreply.github.com>
2022-11-22 19:02:22 +0900

    Update findfunction.pl

--- findfunction.pl
+++ findfunction.pl
@@ -1,4 +1,5 @@
 use strict;
+use warnings;
 
 my $uniprot_brite_path = shift @ARGV;
 my $nr_result_path = shift @ARGV;
@@ -19,16 +20,16 @@ while (<$uniprot_brite>) {
 close $uniprot_brite;
 
 open my $nr_result, "<:utf8", $nr_result_path or die;
-open my $functional_profile, ">:utf8", "func_${file_name}.${file_extension}";
+open my $functional_profile, ">:utf8", "function_${file_name}.${file_extension}";
 my @records;
 while (<$nr_result>) {
     chomp;
     my @fields = split "\t";
-    my $uniprot = $fields[1];
+    my $uniprot = $1 if $fields[1] =~ m/\|(.+)\|/;
     my $id = $fields[0];
     my $start = $fields[6];
     my $end = $fields[7];
-    if ($uniprot_brite_hash{$uniprot} ne "") {
+    if ($uniprot_brite_hash{$uniprot}) {
         print $functional_profile "$id\t$start\t$end\t$uniprot_brite_hash{$uniprot}\n";
     };
     last if eof $nr_result;
